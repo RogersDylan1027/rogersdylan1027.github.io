ef14ef11dfdda732dfaa0e6bd915fc0850d4dfd3
RogersDylan1027 <rogersdylan1027@gmail.com>
2025-12-30 22:55:13 -0500

    Add Update Accuracy.R
    Add accuracy calculation to update the accuracy of "All Results.JSON" automatically

---
+++ Update Accuracy.R
@@ -0,0 +1,21 @@
+library(httr)
+library(jsonlite)
+
+results <- fromJSON("All Results.json", simplifyVector = FALSE)
+
+totalCorrectLines <- 0
+totalLines <- 0
+
+for(x in 2:length(results))
+{
+  for(y in 1:length(results[[x]]$games))
+  {
+    totalCorrectLines <- results[[x]]$games[[y]]$correctLines + totalCorrectLines
+    totalLines <- results[[x]]$games[[y]]$totalLines + totalLines
+  }
+}
+
+results[[1]]$totalCorrectLines <- totalCorrectLines
+results[[1]]$totalLines <- totalLines
+results[[1]]$accuracy <- paste0(round(100*(totalCorrectLines/totalLines), digits = 2), "%")
+write_json(results, "All Results.json", pretty = TRUE, auto_unbox = TRUE)
